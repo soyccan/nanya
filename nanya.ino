9940c99c56f51a058122f0c87c3efc9e178a5058
soyccan <>
2019-05-08 01:44:41 +0800

    final

--- nanya.ino
+++ nanya.ino
@@ -1,3 +1,12 @@
+#define ID 1
+/* ID =
+1 : 戴依柔
+2 : 劉東昕
+3 : 陳仲景
+*/
+
+
+
 #include "pictures.h"
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
@@ -62,28 +71,31 @@ void execute()
     beat = 500;                 //寫拍子所算出的毫秒數
 
     // 測試時謢眼用
-    pixels.setBrightness(30);
+    // pixels.setBrightness(30);
 
     // 第一組
     offset = 0;
-    outColor(24,255,153,204);
-    //outColor(24,255,255,102);
+#if ID == 1 // 戴依柔
+    outColor(24,0,167,255);
+#elif ID == 3 // 陳仲景
+    outColor(24,255,149,0);
+#endif
     output(flash_white, 8);
 
 
     // 正轉
     for(int i=0;i<2;i++)
     {
-      outColor(1, 255,  0,  0);
-      outColor(1, 255,167,  0);
-      outColor(1, 255,255,  0);
-      outColor(1,   0,255,  0);
-      outColor(1,   0,  0,255);
-      outColor(1,   0,211,255);
-      outColor(1, 165,  0,255);
-      outColor(1,   0,  0,  0);
+      outColor(1, 255,   0,   0);
+      outColor(1, 255, 167,   0);
+      outColor(1, 255, 255,   0);
+      outColor(1,   0, 255,   0);
+      outColor(1,   0, 255, 191);
+      outColor(1,  51, 102, 255);
+      outColor(1, 165,   0, 255);
+      outColor(1, 255,  70, 159);
     }
-    
+
     offset = 0;
     output(rainbow_wave, 4);
     output(flower1, 4);
@@ -93,11 +105,8 @@ void execute()
 
     // 蝴蝶
     output(ARROW_red, 4);
-    offset = 88;
     output(ARROW_blue_r, 4);
-    offset = 0;
     output(ARROW_yellow, 4);
-    offset = 88;
     output(num_4, 1);
     output(num_3, 1);
     output(num_2, 1);
@@ -108,35 +117,103 @@ void execute()
     output(fire, 16);
 
     // 停球
-    outColor(16,255,153,204);
-    //outColor(16,255,255,102);
+#if ID == 1 // 戴依柔
+    outColor(16,0,167,255);
+#elif ID == 3 // 陳仲景
+    outColor(16,255,149,0);
+#endif
     // 間奏
     dark(15);
 
- /*   // 換人
+    // 換人
     // 第二組
-    offset = 31;  // 記得改
-    output(redyellowgreen,9);       // 當一份真相   隻手能隱藏
-    output(rainbowlight, 8);    // 直到人們遺忘
-    output(DNA , 8);             // 寫一頁莽撞   我們的篇章 (DNA)
-    output(nest, 8);            // 曾經如此輝煌
-    output(sword, 12);               // 丹青千秋釀 一醉解愁腸 無悔少年枉 (劍)
-    output(flash1, 4);          //只願壯志狂
-    output5in1(note1, note2, note3, note4, note5,16);   //夜未央 天未亮 我在倖存的沙場 只盼望 此生再奔向思念的臉龐
-    output2in1(spiral1,spiral2, 8);                // 淚未乾 心未涼 是什麼依然在滾燙()
-    output(flash1, 4);          // 入陣曲 伴我無悔的狂妄
+    offset = 45;  // 記得改
+    output(redyellowgreen,8);       // 當一份真相   隻手能隱藏
+    output(rainbow2, 8);            // 直到人們遺忘
+    output(DNA , 8);                // 寫一頁莽撞   我們的篇章 (DNA)
+    output(nest, 8);                // 曾經如此輝煌
 
-    outColor(8, 255, 0, 0);     // 忘不記 原不諒 憤恨無疆
-    outColor(8, 0, 255, 0);     // 肅不清 除不盡 魑魅魍魎
-    outColor(8, 255, 255, 0);  // 幼無糧 民無房 誰在分贓
-    outputLongDelay(rainbowchange, 10, 20);                 // 千年後 你我都 仍被豢養 (彩虹變色)
-    outputLongDelay(flowing,12, 20);                 // 間奏(充電)
-    outColor(8, 255, 153, 255);        // 七旋(純色)
-    outColor(8, 255, 102, 0);        // 七旋(純色)
-    outColor(8, 255, 0, 0);        // 七旋(純色)
-    outColor(8, 255, 0, 255);        // 七旋(純色)
+    //丹青千秋釀
+    outColor(1, 0, 60, 255); //藍
+    outColor(1, 0, 255, 0); //綠
+    outColor(1, 255, 128, 0); //黃
+    outColor(1, 255, 0, 0);//紅
 
-*/    // 第一組
+    //一醉解愁腸
+    beat = 250;
+    outColor(1, 255, 255, 255);
+    dark(1);
+    outColor(1, 255, 255, 255);
+    dark(1);
+    outColor(1, 255, 255, 255);
+    dark(1);
+    outColor(1, 255, 255, 255);
+    dark(1);
+    beat = 500;
+
+    //無悔少年枉
+    outColor(1, 0, 60, 255); //藍
+    outColor(1, 0, 255, 0); //綠
+    outColor(1, 255, 128, 0); //黃
+    outColor(1, 255, 0, 0);//紅
+
+
+    //只願壯志狂
+    beat = 250;
+    outColor(1, 255, 153, 153);
+    dark(1);
+    outColor(1, 255, 153, 153);
+    dark(1);
+    outColor(1, 255, 153, 153);
+    dark(1);
+    outColor(1, 255, 153, 153);
+    dark(1);
+    beat = 500;
+
+    //夜未央 天未亮 我在倖存的沙場
+#if ID == 1 // 戴依柔
+    output(fish1, 8);
+#elif ID == 2 // 劉東昕
+    output(fish2, 8);
+#elif ID == 3 // 陳仲景
+    output(fish3, 8);
+#endif
+
+    output3in1(five, zero, zero,8);                      //只盼望 此生再奔向思念的臉龐
+
+    //淚未乾 心未涼 是什麼依然在滾燙
+#if ID == 1 // 戴依柔
+    output(mai, 8);
+#elif ID == 2 // 劉東昕
+    output(shi, 8);
+#elif ID == 3 // 陳仲景
+    output(rai, 8);
+#endif
+
+    //入陣曲 伴我無悔的狂妄
+    beat = 250;
+    outColor(1, 0, 0, 255);
+    dark(1);
+    outColor(1, 0, 0, 255);
+    dark(1);
+    outColor(1, 0, 0, 255);
+    dark(1);
+    outColor(1, 0, 0, 255);
+    dark(1);
+    beat = 500;
+
+    outColor(8, 0, 255, 0);                              // 忘不記 原不諒 憤恨無疆
+    outColor(8, 0, 0, 255);                              // 肅不清 除不盡 魑魅魍魎
+    outColor(8, 255, 255, 0);                            // 幼無糧 民無房 誰在分贓
+    outputLongDelay(rainbowchange, 10, 20);              // 千年後 你我都 仍被豢養 (彩虹變色)
+    dark(8);                                             // 間奏(解繩子)
+    outputLongDelay(flowing, 6, 20);                     // 間奏(充電)
+    output(speed_ribbon1, 8);                            // 七旋
+    output(speed_ribbon2, 8);                            // 七旋
+    output(speed_ribbon3, 8);                            // 七旋
+    output(speed_ribbon1, 7);                            // 七旋
+
+    // 第一組
     // 單八
     // (夜末央)
     offset = 0;
@@ -146,16 +223,21 @@ void execute()
     outColor(1,0, 0, 0);
     output(flower1, 12);
 
+    offset = 45; // 記得改
+    output(heart, 8);                                                 // 淚未乾 心未涼 是什麼依然在滾燙
+    output(sixstar, 8);                                               // 入陣曲  四面楚歌誰獨唱
+    output5in1(NANYO_N1, NANYO_A, NANYO_N2, NANYO_Y, NANYO_O, 16);    // 夜已央 天已亮 白晝隱沒了星光 像我們 都終將 葬身歷史的洪荒
+    output(rainbowspiral, 8);                                         // 當世人 都遺忘 我血液曾為誰滾燙
 
-    offset = 31; // 記得改
-    //outColor(16, 255, 255, 255);                             // 夜未央 天未亮 我在倖存的沙場
-                                 // 只盼望 此生再 奔向思念的臉龐
-    output(heart, 8);            // 淚未乾 心未涼 是什麼依然在滾燙
-    output(sixstar, 8);               // 入陣曲  四面楚歌誰獨唱
-    output5in1(NANYO_N1, NANYO_A, NANYO_N2, NANYO_Y, NANYO_O, 16);   // 夜已央 天已亮 白晝隱沒了星光 像我們 都終將 葬身歷史的洪荒
-    output(rainbowspiral, 8);   // 當世人 都遺忘 我血液曾為誰滾燙
-    output(flash1, 8);          // 入陣曲 伴我無悔的狂妄
-    outColor(14, 255, 255, 255);    // 入陣去 只因 恨鐵不成鋼
+    // 入陣曲 伴我無悔的狂妄
+    beat = 250;
+    for(int j = 0;j < 8;j++){
+    outColor(1, 255, 255, 255);
+    dark(1);
+    }
+    beat = 500;
+
+    outColor(14, 255, 255, 255);                                      // 入陣去 只因 恨鐵不成鋼
 
     dark(10000);
 }
@@ -220,6 +302,48 @@ void output2in1(int picturenumber1, int picturenumber2, int beatnum)
               for (int k = 0; k < NUMPIXELS; k++)
               {
                   readPGM(picturenumber2,j,k);
+                  pixels.setPixelColor(k, pixels.Color(table[j][k][r], table[j][k][g], table[j][k][b])); //一次亮一排
+              }
+              pixels.show();
+              delay(2);
+          }
+          currenttime = millis();
+    }while( currenttime < proess );
+}
+
+void output3in1(int picturenumber1, int picturenumber2, int picturenumber3, int beatnum)
+{
+    picturenumber1 += offset;
+    picturenumber2 += offset;
+    picturenumber3 += offset;
+    proess = proess + (beatnum*beat);
+    do
+    {
+          for (int j = 0; j < NUMLINES; j++)
+          {
+              for (int k = 0; k < NUMPIXELS; k++)
+              {
+                  readPGM(picturenumber1,j,k);
+                  pixels.setPixelColor(k, pixels.Color(table[j][k][r], table[j][k][g], table[j][k][b])); //一次亮一排
+              }
+              pixels.show();
+              delay(1);
+          }
+          for (int j = 0; j < NUMLINES; j++)
+          {
+              for (int k = 0; k < NUMPIXELS; k++)
+              {
+                  readPGM(picturenumber2,j,k);
+                  pixels.setPixelColor(k, pixels.Color(table[j][k][r], table[j][k][g], table[j][k][b])); //一次亮一排
+              }
+              pixels.show();
+              delay(1);
+          }
+          for (int j = 0; j < NUMLINES; j++)
+          {
+              for (int k = 0; k < NUMPIXELS; k++)
+              {
+                  readPGM(picturenumber3,j,k);
                   pixels.setPixelColor(k, pixels.Color(table[j][k][r], table[j][k][g], table[j][k][b])); //一次亮一排
               }
               pixels.show();
